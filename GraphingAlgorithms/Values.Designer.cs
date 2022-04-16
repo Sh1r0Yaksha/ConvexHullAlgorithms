2d6d056e0ca431c5326aff042f50590d08a39b83
Shir0Yaksha <85480331+Sh1r0Yaksha@users.noreply.github.com>
2022-04-16 12:02:49 +0530

    Jarvis March

--- GraphingAlgorithms/Values.Designer.cs
+++ GraphingAlgorithms/Values.Designer.cs
@@ -33,14 +33,6 @@
             this.label2 = new System.Windows.Forms.Label();
             this.coordNumApplyButton = new System.Windows.Forms.Button();
             this.coordNumTextBox = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -64,9 +56,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 76);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(318, 20);
+            this.label2.Size = new System.Drawing.Size(198, 20);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Enter number of coordinates to show on graph";
+            this.label2.Text = "Enter number of coordinates";
             // 
             // coordNumApplyButton
             // 
@@ -76,6 +68,7 @@
             this.coordNumApplyButton.TabIndex = 3;
             this.coordNumApplyButton.Text = "Apply";
             this.coordNumApplyButton.UseVisualStyleBackColor = true;
+            this.coordNumApplyButton.Click += new System.EventHandler(this.coordNumApplyButton_Click);
             // 
             // coordNumTextBox
             // 
@@ -84,83 +77,12 @@
             this.coordNumTextBox.Size = new System.Drawing.Size(125, 27);
             this.coordNumTextBox.TabIndex = 4;
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(42, 152);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(29, 20);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "X1:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 175);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(80, 27);
-            this.textBox1.TabIndex = 6;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(126, 152);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(28, 20);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Y1:";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(98, 175);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(80, 27);
-            this.textBox2.TabIndex = 8;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(42, 205);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(29, 20);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "X1:";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(125, 205);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(29, 20);
-            this.label6.TabIndex = 10;
-            this.label6.Text = "X1:";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(12, 228);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(80, 27);
-            this.textBox3.TabIndex = 11;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(98, 228);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(80, 27);
-            this.textBox4.TabIndex = 12;
-            // 
             // Values
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(539, 551);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label3);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(267, 551);
             this.Controls.Add(this.coordNumTextBox);
             this.Controls.Add(this.coordNumApplyButton);
             this.Controls.Add(this.label2);
@@ -180,13 +102,5 @@
         private Label label2;
         private Button coordNumApplyButton;
         private TextBox coordNumTextBox;
-        private Label label3;
-        private TextBox textBox1;
-        private Label label4;
-        private TextBox textBox2;
-        private Label label5;
-        private Label label6;
-        private TextBox textBox3;
-        private TextBox textBox4;
     }
 }
--- GraphingAlgorithms/Values.cs
+++ GraphingAlgorithms/Values.cs
@@ -2,9 +2,109 @@ namespace GraphingAlgorithms
 {
     public partial class Values : Form
     {
+        TextBox[] xTextBox;
+        TextBox[] yTextBox;
+
+        public static Point[] points;
+        public static int scaleValue;
+
+        int numOfCoordinates = new int();
+        Button jarvisMarchButton = new Button();
+        Button grahamScanButton = new Button();
+
         public Values()
         {
             InitializeComponent();
         }
+
+        private void coordNumApplyButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                numOfCoordinates = int.Parse(coordNumTextBox.Text);
+
+                xTextBox = new TextBox[numOfCoordinates];
+                yTextBox = new TextBox[numOfCoordinates];
+
+                int initialLabelYLocation = 152;
+                int initialTextBoxYLocation = 175;
+
+                for (int i = 0; i < numOfCoordinates; i++)
+                {
+                    Label labelX = new Label();
+                    this.Controls.Add(labelX);
+                    labelX.Text = $"X{i + 1}:";
+                    labelX.Location = new Point(35, initialLabelYLocation + i * 53);
+
+                    Label labelY = new Label();
+                    this.Controls.Add(labelY);
+                    labelY.Text = $"Y{i + 1}:";
+                    labelY.TextAlign = ContentAlignment.TopRight;
+                    labelY.Location = new Point(labelX.Location.X + 80, initialLabelYLocation + i * 53);
+
+                    xTextBox[i] = new TextBox();
+                    this.Controls.Add(xTextBox[i]);
+                    xTextBox[i].Location = new Point(12, initialTextBoxYLocation + i * 53);
+                    xTextBox[i].Size = new Size(80, 27);
+
+                    yTextBox[i] = new TextBox();
+                    this.Controls.Add(yTextBox[i]);
+                    yTextBox[i].Location = new Point(xTextBox[i].Location.X + 152, xTextBox[i].Location.Y);
+                    yTextBox[i].Size = new Size(80, 27);                 
+                }
+
+                this.Controls.Add(jarvisMarchButton);
+                jarvisMarchButton.Location = new Point(xTextBox[numOfCoordinates - 1].Location.X, xTextBox[numOfCoordinates - 1].Location.Y + 53);
+                jarvisMarchButton.Text = "JM";
+                jarvisMarchButton.Size = new Size(80, 27);
+                jarvisMarchButton.Click += JM_Click;
+
+                grahamScanButton.Location = new Point(yTextBox[numOfCoordinates - 1].Location.X, yTextBox[numOfCoordinates - 1].Location.Y + 53);
+                this.Controls.Add(grahamScanButton);
+                grahamScanButton.Text = "GM";
+                grahamScanButton.Size = new Size(80, 27);
+                grahamScanButton.Click += GS_Click;
+
+                scaleValue = int.Parse(scalingFactorTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void JM_Click(object sender, EventArgs e)
+        {
+            AddValues();
+            JarvisMarch jm = new JarvisMarch();
+            jm.ShowDialog();
+        }
+
+        private void GS_Click(object sender, EventArgs e)
+        {
+            AddValues();
+            GrahamScan gs = new GrahamScan();
+            gs.ShowDialog();
+        }
+
+        private void AddValues()
+        {
+            try
+            {
+                for (int i = 0; i < numOfCoordinates; i++)
+                {
+                    points[i].X = int.Parse(xTextBox[i].Text);
+                    points[i].Y = int.Parse(yTextBox[i].Text);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+
     }
 }
